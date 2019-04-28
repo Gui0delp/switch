d3a243b4b0d5d94c04c4091ee21afb09c2977c91
Gui0delp <delpro.guillaume@gmail.com>
2019-04-28 20:59:23 +0200

    Content the mai code of the game

---
+++ main.py
@@ -0,0 +1,12 @@
+"""Name of the game: switch
+"""
+
+def main():
+    """The function contents the main code of the game
+    """
+
+    print("Main function")
+
+if __name__ == "__main__":
+    #Call the main() function of the game
+    main()
