b8c964713a4e881f37a56257a6311c88f54d85f5
Gui0delp <delpro.guillaume@gmail.com>
2019-04-28 21:33:24 +0200

    Add some stuff iin the differents modules

---
+++ modules/character.py
@@ -0,0 +1,9 @@
+"""Contains all the function for the character
+"""
+
+class Character(object):
+    """N/A
+    """
+
+    def __init__(self):
+        pass
---
+++ modules/level.py
@@ -0,0 +1,9 @@
+"""Contains all the function for the level
+"""
+
+class Level(object):
+    """N/A
+    """
+
+    def __init__(self):
+        pass
