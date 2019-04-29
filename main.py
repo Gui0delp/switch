dac5d208da242ef4806aae6a340db64ccb28e740
Gui0delp <delpro.guillaume@gmail.com>
2019-04-29 19:32:38 +0200

    Add the main structure of the code and import pygame module fro show the game level

--- main.py
+++ main.py
@@ -6,11 +6,34 @@
 
 import pygame
 import modules.level as lvl
-import modules.character as user
 
 def main():
     """The function contains the main code of the game
     """
+    w_sprite = 32
+    l_sprite = 32
+    w_width = 9 * w_sprite
+    w_length = 9 * l_sprite
+    quit_game = True
+    pygame.init() # pylint: disable=no-member
+    pygame.display.set_caption("Macgyver maze") # pylint: disable=no-member
+    window = pygame.display.set_mode((w_width, w_length)) # pylint: disable=no-member
+    level = lvl.Level()
+    level.generate_lvl()
+
+    while quit_game:
+
+        level.refresh_lvl(window)
+        pygame.display.flip()
+
+        for event in pygame.event.get():
+            if event.type == pygame.KEYDOWN:    # pylint: disable=no-member
+                if event.key == pygame.K_ESCAPE:    # pylint: disable=no-member
+                    quit_game = False
+                    pygame.quit() # pylint: disable=no-member
+
+
+    input()
 
 if __name__ == "__main__":
     #Call the main() function of the game
