fc68ecf0ceac0416eeaeeec53b267e8163f2983d
Gui0delp <delpro.guillaume@gmail.com>
2019-05-01 16:41:26 +0200

    Add FIFO structure for capture the movement of the user

--- main.py
+++ main.py
@@ -6,6 +6,7 @@
 
 import pygame
 import modules.level as lvl
+import modules.character as user
 
 def main():
     """The function contains the main code of the game
@@ -16,21 +17,38 @@ def main():
     w_length = 9 * l_sprite
     quit_game = True
     pygame.init() # pylint: disable=no-member
-    pygame.display.set_caption("Macgyver maze") # pylint: disable=no-member
+    pygame.display.set_caption("Switch") # pylint: disable=no-member
     window = pygame.display.set_mode((w_width, w_length)) # pylint: disable=no-member
     level = lvl.Level()
     level.generate_lvl()
+    player = user.Character(level.game_level)
 
     while quit_game:
 
         level.refresh_lvl(window)
-        pygame.display.flip()
+        player.move(window, "")
 
         for event in pygame.event.get():
             if event.type == pygame.KEYDOWN:    # pylint: disable=no-member
                 if event.key == pygame.K_ESCAPE:    # pylint: disable=no-member
                     quit_game = False
                     pygame.quit() # pylint: disable=no-member
+                elif event.key == pygame.K_LEFT: # pylint: disable=no-member
+                    player.move(window, "LEFT")
+                    player.switch()
+                elif event.key == pygame.K_RIGHT: # pylint: disable=no-member
+                    player.move(window, "RIGHT")
+                    player.switch()
+                elif event.key == pygame.K_UP: # pylint: disable=no-member
+                    player.move(window, "UP")
+                    player.switch()
+                elif event.key == pygame.K_DOWN: # pylint: disable=no-member
+                    player.move(window, "DOWN")
+                    player.switch()
+                else:
+                    pass
+
+        pygame.display.flip()
 
 if __name__ == "__main__":
     #Call the main() function of the game
